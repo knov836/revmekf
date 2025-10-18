modifications in computation of final rot in linalg correct

kfilterdata_synthetic.py
@@ -100,6 +100,7 @@ class KFilterDataSynth:
         self.grav_earth = self.calc_grav_earth(self.speed_earth)
         self.gravs = self.mems_ref(self.grav_earth)
         self.mag0 = np.array([0,1,0],dtype=mpf)
+        self.mag0 = np.array([1,0,0],dtype=mpf)
         
         
     def gyro_data(self,orient):
@@ -139,13 +140,14 @@ class KFilterDataSynth:
         for i in range(len(self.orient)):
             acc[i,:]=self.gravity
 
-        acc[:acc_earth.shape[0]] = acc_earth
+        #acc[:acc_earth.shape[0]] = acc_earth
         return acc
     
     def calc_mag_earth(self):
         mag= np.zeros((len(self.orient),3),dtype=mpf)
         for i in range(len(self.orient)):
-            mag[i,1]=mpf(1)
+            #mag[i,1]=mpf(1)
+            mag[i,0]=mpf(1)
         return mag
     def new_orient(self):
         new_orient = np.zeros((self.size,4),dtype=mpf)
@@ -157,6 +159,7 @@ class KFilterDataSynth:
             m = m/mp.norm(m)
             m0 = np.array([0,1,0],dtype=mpf)
             M = np.array([-skewSymmetric(a)@m,m,a]).T
+            M = np.array([m,skewSymmetric(a)@m,a]).T
             new_orient[i,:] = normalize(quat_inv(RotToQuat(M)))
         return new_orient
     
@@ -173,7 +176,7 @@ class KFilterDataSynth:
 N=100
 angle = int(N/2)
 newset = KFilterDataSynth(N,mpf(0.1),mode='GyroAccMag') 
-newset = KFilterDataSynth(N,mpf(0.1),mode='OdoAccPre') 
+#newset = KFilterDataSynth(N,mpf(0.1),mode='OdoAccPre') 
 
 fig = plt.figure()
 ax = fig.add_axes([0,0,1,1])

linalg.py
@@ -935,6 +935,8 @@ def acc_from_normal_imu_grav(norm0,norm,acc,grav,normal,center,start=[0,0,1],s_r
         x = (depth-center[1])/direc[1]
         P1 = x*direc+center
         
+        R = mp.norm(pracc)
+        Rg = mp.norm(rgrav)
         
         if mp.norm(P1)>R:
             beta = mpf(0)
@@ -942,6 +944,17 @@ def acc_from_normal_imu_grav(norm0,norm,acc,grav,normal,center,start=[0,0,1],s_r
             beta = mp.acos(mp.norm(P1)/R)
         Q0 = np.array(P1+R*mp.sin(beta)*L1,dtype=mpf)
         Q1 = np.array(P1-R*mp.sin(beta)*L1,dtype=mpf)
+        
+        
+        
+        if mp.norm(P1)>Rg:
+            betag = mpf(0)
+            t4t0g = t_v0
+        else:
+            betag = mp.acos(mp.norm(P1)/Rg)
+            #beta = mp.acos(mp.norm([P1[0],P1[2]])/R)
+        Qg0 = np.array(P1+Rg*mp.sin(betag)*L1,dtype=mpf)
+        Qg1 = np.array(P1-Rg*mp.sin(betag)*L1,dtype=mpf)
         
         
     else:
@@ -983,6 +996,7 @@ def acc_from_normal_imu_grav(norm0,norm,acc,grav,normal,center,start=[0,0,1],s_r
             Qg0 = np.array(P1+Rg*mp.sin(betag)*L1,dtype=mpf)
             Qg1 = np.array(P1-Rg*mp.sin(betag)*L1,dtype=mpf)
         else:
+            print(normal)
             Q0 = racc
             Q1 = racc
             t4t0 = t_v0
@@ -1199,7 +1213,7 @@ def acc_from_normal_imu_grav(norm0,norm,acc,grav,normal,center,start=[0,0,1],s_r
          l_HH.evalf(subs={v: float(t2t0 - t_v0)})]).astype(float)
         
         labels = ["MEKF", "Theta 1", "Theta 2"]
-        tt = np.max([np.abs(float(t4t0)),np.abs(float(t2t0))])
+        tt = np.max([np.abs(float(t4t0-t_v0)),np.abs(float(t2t0-t_v0))])
         f = sym.lambdify(v, l_HH, 'numpy')
         vv = np.linspace(-tt, tt, 1000)
         fig, ax = plt.subplots(figsize=(6,4))

solver_kalman.py
@@ -162,7 +162,7 @@ class SolverFilterPlan:
         
         qq = (self.quaternion[i, :])
         if newset==None:
-            KFilter.UpdateSensor(mpf(time),Surface,acc,gyro, mag,self.orient[i,:])
+            KFilter.UpdateSensor(mpf(time),Surface,acc,gyro, mag,self.orient[i+1,:])
         else:
             """if self.mode.startswith('GyroAccMag'):
                 KFilter.UpdateSensor(mpf(time),Surface, acc,gyro,mag,self.orient[i,:])
@@ -189,7 +189,7 @@ class SolverFilterPlan:
                 if "model" in kargs.keys():
                     model = kargs["model"]        
                 Surface = np.array(np.concatenate(([0,],normal.astype(mpf),np.zeros(6))),dtype=mpf)
-                KFilter.UpdateSensor(time,Surface, acc,gyro,mag,newset.orient[i,:],std_acc_z = std_acc_z,correction = correction,xtensor = xtensor,model=model)
+                KFilter.UpdateSensor(time,Surface, acc,gyro,mag,newset.orient[i+1,:],std_acc_z = std_acc_z,correction = correction,xtensor = xtensor,model=model)
             elif self.mode == 'GyroAcc':
                 gyro, acc, normal= args[:3]
                 Surface = np.array(np.concatenate(([0,],normal.astype(mpf),np.zeros(6))),dtype=mpf)
@@ -197,7 +197,7 @@ class SolverFilterPlan:
             elif self.mode == 'OdoAccPre':
                 acc,pressure,leftw,rightw= args[:4]
                 Surface = np.array(np.concatenate(([0,],normal.astype(mpf),np.zeros(6))),dtype=mpf)
-                KFilter.UpdateSensor(time,Surface,acc, pressure,leftw,rightw,self.orient[i,:])
+                KFilter.UpdateSensor(time,Surface,acc, pressure,leftw,rightw,self.orient[i+1,:])
         self.gravity_r[i+1, :] = KFilter.gravity_r[0:4]
         
         #KFilter.UpdateSensor(mpf(time),gyro, acc,mag,None,Surface)

variants/reversible_gyroaccmag.py
@@ -50,7 +50,7 @@ class PredictFilter(Filter):
         
         grav= np.copy(Accelerometer)
         #if np.abs(self.std_acc_z)>0.85:
-        grav[2] = normal[2]*np.linalg.norm(self.gravity)
+        #grav[2] = normal[2]*np.linalg.norm(self.gravity)
         #print(acc[2],Accelerometer[2],normal)
         #nAccelerometer = np.array(quat_rot([0,*acc],self.Quaternion))[1:4]
         
@@ -69,10 +69,20 @@ class PredictFilter(Filter):
         mm1 = np.array([0,1,0],dtype=mpf)
         quat= quat_ntom(mm0,mm1)
         
-        qq = quat_rot(self.Quaternion,(quat))
+        mm2 = np.array(self.mag0,dtype=mpf)
+        mag0 = np.copy(self.mag0)
+        mag0[2] = 0
+        mag0 = mag0/np.linalg.norm(mag0)
+        quat2 = quat_ntom(mag0,mm1)
+        """qq = quat_mult(self.Quaternion,quat_inv(quat))
+        logq1 = log_q(np.array(self.Quaternion))
+        qq0 = quat_ntom(logq1/np.linalg.norm(logq1),mm2)
+        qq1 = quat_rot(self.Quaternion,qq0)
+        qq2 = np.array(quat_mult(qq1,quat_ntom(self.mag0,mm1)))"""
+        qq = quat_mult(quat_mult(quat2,self.Quaternion),quat_inv(quat))
         logrot1 = log_q(np.array(qq))
         #logrot1 = np.zeros(3)
-        #pdb.set_trace()
+        
         if self.neural:
             
             acc1,rot1,irot1,acc2,rot2,irot2,acc3,rot3,irot3,corrected,not_corrected,angle_acc,t0,t2,t3,t4,et0,et2,et3,et4= acc_from_normal_imu_grav_neural(np.array(quat_rot([0,*np.array(nAxis,dtype=mpf)],quat))[1:4],np.array([0,1,0],dtype=mpf) , np.array(quat_rot([0,*(nAccelerometer*(self.dt**2))],quat))[1:4],np.array(quat_rot([0,*(grav*(self.dt**2))],quat))[1:4], normal, self.surf_center,start = logrot1,heuristic=self.heuristic,correction = False)
@@ -130,8 +140,8 @@ class PredictFilter(Filter):
                 acc1,rot1,irot1,acc2,rot2,irot2,acc3,rot3,irot3,corrected,not_corrected,label,angle_acc,t0,t2,t3,t4,et0,et2,et3,et4= acc_from_normal_imu_grav_manual(np.array(quat_rot([0,*np.array(nAxis,dtype=mpf)],quat))[1:4],np.array([0,1,0],dtype=mpf) , np.array(quat_rot([0,*(nAccelerometer*(self.dt**2))],quat))[1:4],np.array(quat_rot([0,*(grav*(self.dt**2))],quat))[1:4], normal, self.surf_center,start = logrot1,heuristic=self.heuristic,correction = self.correction)
                 self.label=label
             else:
-                
-                acc1,rot1,irot1,acc2,rot2,irot2,acc3,rot3,irot3,corrected,not_corrected,angle_acc,t0,t2,t3,t4,et0,et2,et3,et4= acc_from_normal_imu_grav(np.array(quat_rot([0,*np.array(nAxis,dtype=mpf)],quat))[1:4],np.array([0,1,0],dtype=mpf) , np.array(quat_rot([0,*(nAccelerometer*(self.dt**2))],quat))[1:4],np.array(quat_rot([0,*(grav*(self.dt**2))],quat))[1:4], normal, self.surf_center,start = logrot1,heuristic=self.heuristic,correction = self.correction)
+                #pdb.set_trace()
+                acc1,rot1,irot1,acc2,rot2,irot2,acc3,rot3,irot3,corrected,not_corrected,angle_acc,t0,t2,t3,t4,et0,et2,et3,et4= acc_from_normal_imu_grav(np.array(quat_rot([0,*np.array(nAxis,dtype=mpf)],quat))[1:4],np.array([0,1,0],dtype=mpf) , np.array(quat_rot([0,*(nAccelerometer*(self.dt**2))],quat))[1:4],np.array(quat_rot([0,*(grav*(self.dt**2))],quat))[1:4], np.array(quat_rot([0,*normal],(quat2)))[1:4], np.array(quat_rot([0,*self.surf_center],(quat2)))[1:4],start = logrot1,heuristic=self.heuristic,correction = self.correction)
             self.t0 = t0
             self.t2 = t2
             self.t3 = t3
@@ -148,7 +158,8 @@ class PredictFilter(Filter):
             self.not_corrected = True
 
         acc = np.array(quat_rot([0,*(acc1)],quat_inv(quat)),dtype=mpf)[1:4]
-        rot = QuatToRot(quat_inv(quat))@rot1
+        rot = QuatToRot(quat_inv(quat))@rot1@QuatToRot((quat2))
+        #pdb.set_trace()
         return acc
     
     def update(self,Gyroscope, Accelerometer,Magnetometer,Orient):